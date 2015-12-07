3580e42f9863f0c59149e6fbf745b6cf7de8bad0
Cameron Lengerich <cameron.lengerich@gmail.com>
2015-12-07 16:04:57 -0500

    Verifys date before posting Adds post method Adds main method for groupme.py Removed ghostdriver.log Removes debugging screenshot

--- gmail.py
+++ gmail.py
@@ -1,24 +1,22 @@
 import sqlite3
 from selenium import webdriver
+import datetime as dt
 import time
 import re
 import os
 
 
 def _get_credentials():
-    dirname = os.path.dirname(__file__)
-    conn = sqlite3.connect(os.path.join(dirname, '../main'))
+    conn = sqlite3.connect('main')
 
     res = conn.execute('select * from logins where site == "Google"')
-
     info = res.fetchone()
-
     email, password = info[1:]
 
     return email, password
 
 
-def _login(driver, email, password):
+def _login(email, password):
     driver.find_element_by_id('Email').send_keys(email)
     driver.find_element_by_id('next').click()
     time.sleep(1)
@@ -27,13 +25,13 @@ def _login(driver, email, password):
     time.sleep(1)
 
 
-def _query(driver):
+def _query():
     driver.find_element_by_id('gbqfq').send_keys('subject:spiritual cyber-vitamin')
     driver.find_element_by_id('gbqfb').click()
     time.sleep(1)
 
 
-def _open_last_email(driver):
+def _open_last_email():
     tables = driver.find_elements_by_tag_name('tbody')
 
     for table in tables:
@@ -44,7 +42,7 @@ def _open_last_email(driver):
             return
 
 
-def _find_room(driver):
+def _find_room():
     ulists = driver.find_elements_by_tag_name('ul')
 
     for ul in ulists:
@@ -66,24 +64,50 @@ def _find_room(driver):
                 return None
 
 
+def _find_date():
+    spans = driver.find_elements_by_tag_name('span')
+
+    for span in spans:
+        date_pattern = '.+day, .+ \d\d?, \d{4}'
+        matches = re.findall(date_pattern, span.text)
+        if matches:
+            # ['Monday, December 7, 2015'], ... -> ['December', '7']
+            date = matches[0].split(', ')[1].split()
+            if len(date[1]) == 1:
+                date[1] = '0' + str(date[1])
+            return date
+
+
+def _verify_date(ext_date):
+    today_day = dt.datetime.today().strftime('%d')
+    today_month = dt.datetime.today().strftime('%B')
+    today = [today_month, today_day]
+    if today == ext_date:
+        return True
+    return False
+
+
 def find_room():
     # initialize driver and set to url
-    driver = webdriver.PhantomJS(service_log_path=os.path.devnull)
     url = 'https://mail.google.com/mail/u/0/#inbox'
     driver.get(url)
 
     email, password = _get_credentials()
 
-    _login(driver, email, password)
+    _login(email, password)
 
-    _query(driver)
+    _query()
 
-    _open_last_email(driver)
+    _open_last_email()
 
-    room = _find_room(driver)
+    room = _find_room()
 
-    return room
+    date = _find_date()
+
+    if _verify_date(date):
+        return room
 
 
+driver = webdriver.PhantomJS(service_log_path=os.path.devnull)
 if __name__ == '__main__':
     find_room()
--- groupme.py
+++ groupme.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-
 import requests
 import gmail
 
 
-base_url = 'https://api.groupme.com/v3'
-access_token = '6d9ab8c07df50133342e1546f031de51'
-bot_id = 'eac5909e675c05af67a1e2c755'
-
-room = gmail.find_room()
-if room:
-    message = 'Today\'s meeting will be held in Walb %s' % room
-    payload = {'bot_id': bot_id, 'text': message}
+def post(payload):
     requests.post('%s/bots/post' % base_url, data=payload)
+
+
+if __name__ == '__main__':
+    base_url = 'https://api.groupme.com/v3'
+    bot_id = 'eac5909e675c05af67a1e2c755'
+
+    room = gmail.find_room()
+    if room:
+        message = 'Today\'s meeting will be held in Walb %s' % room
+        payload = {'bot_id': bot_id, 'text': message}
+        post(payload)
